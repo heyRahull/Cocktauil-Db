2f8d5cbd5e67d531b6a2b6b305845b151a83c9b5
Rahul Agarwal <rahul123agarwal99@gmail.com>
2021-05-22 22:37:42 +0530

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Choose your favoriate cocktail from a variety of cocktails, you can get
+        the complete information about a cocktail you can even search for a
+        cocktail of your choice in the sdearch bar and if it is present in the
+        database you can have a complete information about it. Most of the
+        enduring classic cocktails comprise just three ingredients eg.
+        rum+lime+sugar=Daiquiri. A few, such as the Negroni consists of three
+        equal parts. It's incredible how many great three-ingredient cocktails
+        are there.
+      </p>
+    </section>
+  );
+};
+
+export default About;
